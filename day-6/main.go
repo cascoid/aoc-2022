11ed6d742cd56c2b110b87d4df3b23f8a60f7da6
Reece <cascoidmc@gmail.com>
2022-12-06 17:52:00 +0000

    Day 6

---
+++ day-6/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+var input []byte
+
+func main() {
+	i, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input = i
+
+	partOne()
+	partTwo()
+}
+
+func partOne() {
+	r := findUniqueCharSequence(4)
+	fmt.Println(r)
+}
+
+func partTwo() {
+	r := findUniqueCharSequence(14)
+	fmt.Println(r)
+}
+
+func findUniqueCharSequence(markerLength int) int {
+	prevChars := []byte{}
+	for i := 0; i < len(input); i++ {
+		r := input[i]
+		index := contains(prevChars, r)
+		if index != -1 {
+			prevChars = prevChars[index+1:]
+		}
+		prevChars = append(prevChars, r)
+		if len(prevChars) == markerLength {
+			return i + 1
+		}
+	}
+
+	return -1
+}
+
+func contains(arr []byte, elem byte) int {
+	for i, a := range arr {
+		if a == elem {
+			return i
+		}
+	}
+	return -1
+}
